1834f30e0ae13ef6270584603712a00d3181803f
Basile Dura <basile@bdura.me>
2024-01-13 16:45:01 +0100

    feat: parse header

---
+++ src/parsers/comment.rs
@@ -0,0 +1,34 @@
+use nom::{
+    bytes::complete::{tag, take_till},
+    sequence::Tuple,
+    IResult,
+};
+
+pub fn parse_comment(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    let (input, (_, content, _)) =
+        (tag(b"%"), take_till(|c| c == b'\n'), tag(b"\n")).parse(input)?;
+
+    Ok((input, content))
+}
+
+#[derive(Debug)]
+pub struct Comment(pub String);
+
+impl Comment {
+    pub fn parse(input: &[u8]) -> IResult<&[u8], Self> {
+        let (input, content) = parse_comment(input)?;
+        let comment = Self(String::from_utf8(content.to_vec()).unwrap());
+        Ok((input, comment))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_comment() {
+        let (_, comment) = Comment::parse(b"%this is a test\n").unwrap();
+        assert_eq!(comment.0, "this is a test".to_owned())
+    }
+}
--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,21 +1,27 @@
-mod version;
-use version::Version;
+mod header;
+pub use header::Header;
+
+pub mod object;
+
+mod comment;
+pub use comment::Comment;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Pdf {
-    pub version: Version,
+    pub header: Header,
 }
 
 impl Pdf {
     pub fn parse(input: &[u8]) -> Result<Self, String> {
-        let (_, version) =
-            Version::parse(input).map_err(|e| format!("Error while parsing: {e}"))?;
-        Ok(Self { version })
+        let (_, header) = Header::parse(input).map_err(|e| format!("Error while parsing: {e}"))?;
+        Ok(Self { header })
     }
 }
 
 #[cfg(test)]
 mod tests {
+
+    use crate::parsers::header::Version;
 
     use super::*;
 
@@ -26,7 +32,10 @@ mod tests {
         assert_eq!(
             pdf,
             Pdf {
-                version: Version::Pdf14
+                header: Header {
+                    version: Version::Pdf14,
+                    binary: true
+                }
             }
         );
     }
