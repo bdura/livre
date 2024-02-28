2a4992d1d80e692a23050ebe27e12f4ae2b72cf0
Basile Dura <basile@bdura.me>
2024-02-28 09:21:06 +0100

    feat: clean octal and hex parsing

--- src/utilities/digits.rs
+++ src/utilities/digits.rs
@@ -2,10 +2,12 @@ use std::{fmt::Debug, str::FromStr};
 
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take},
-    character::complete::{digit1, oct_digit1},
-    combinator::map,
-    IResult,
+    bytes::complete::tag,
+    character::complete::{digit0, digit1, hex_digit1, oct_digit1},
+    combinator::{opt, recognize},
+    error::{Error, ErrorKind, ParseError},
+    sequence::separated_pair,
+    Err, IResult,
 };
 
 /// Parse up to 3 bytes to get the number represented by the underlying octal code.
@@ -13,32 +15,59 @@ pub fn parse_octal(input: &[u8]) -> IResult<&[u8], u8> {
     let value = &input[..input.len().min(3)];
     let (_, num) = oct_digit1(value)?;
 
-    let input = &input[num.len()..];
+    let len = num.len();
 
+    if len == 0 {
+        return Err(Err::Error(Error::from_error_kind(
+            input,
+            ErrorKind::OctDigit,
+        )));
+    }
+
+    let input = &input[len..];
+
+    // SAFETY: `num` only contains octal digits,
+    // and is therefore both utf8-encoded and parseable
     let s = unsafe { std::str::from_utf8_unchecked(num) };
     let n = u8::from_str_radix(s, 8).expect("We know it's a valid number.");
 
     Ok((input, n))
 }
 
+/// Parse up to two bytes to get the number represented by the hexadecimal code.
 pub fn parse_hexadecimal_bigram(input: &[u8]) -> IResult<&[u8], u8> {
-    fn inner(input: &[u8]) -> u8 {
-        let len = input.len();
+    // TODO: when we go "streaming"
+    // let n = input.len();
+    // if n < 2 {
+    //     // SAFETY: n < 2, therefore 2 - n is non-zero.
+    //     let needed = unsafe { NonZeroUsize::new_unchecked(2 - n) };
+    //     return Err(nom::Err::Incomplete(nom::Needed::Size(needed)));
+    // }
 
-        let mut res = {
-            // TODO: check the invariant.
-            let num = std::str::from_utf8(input).unwrap();
-            u8::from_str_radix(num, 16).unwrap()
-        };
+    let value = &input[..input.len().min(2)];
+    let (_, num) = hex_digit1(value)?;
 
-        if len == 1 {
-            res *= 16;
-        }
+    let len = num.len();
 
-        res
+    if len == 0 {
+        return Err(Err::Error(Error::from_error_kind(
+            input,
+            ErrorKind::HexDigit,
+        )));
     }
 
-    alt((map(take(2usize), inner), map(take(1usize), inner)))(input)
+    let input = &input[len..];
+
+    // SAFETY: `num` only contains hex digits,
+    // and is therefore both utf8-encoded and parseable
+    let s = unsafe { std::str::from_utf8_unchecked(num) };
+    let mut n = u8::from_str_radix(s, 16).expect("We know it's a valid number.");
+
+    if len == 1 {
+        n *= 16;
+    }
+
+    Ok((input, n))
 }
 
 pub fn parse_digits<O, E>(input: &[u8]) -> IResult<&[u8], O>
@@ -58,4 +87,45 @@ where
 
 pub fn parse_sign(input: &[u8]) -> IResult<&[u8], &[u8]> {
     alt((tag("+"), tag("-")))(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case(b"001", 1)]
+    #[case(b"010", 8)]
+    #[case(b"10", 8)] // For octal numbers, missing zeros are prepended
+    fn octal(#[case] input: &[u8], #[case] result: u8) {
+        let (_, n) = parse_octal(input).unwrap();
+        assert_eq!(n, result);
+    }
+
+    #[rstest]
+    #[case(b"01", 1)]
+    #[case(b"10", 16)]
+    #[case(b"1", 16)] // For hex numbers, missing zeros are appended
+    fn hex(#[case] input: &[u8], #[case] result: u8) {
+        let (_, n) = parse_hexadecimal_bigram(input).unwrap();
+        assert_eq!(n, result);
+    }
+
+    #[rstest]
+    #[case(b"")]
+    #[case(b"99")]
+    #[should_panic]
+    fn octal_failure(#[case] input: &[u8]) {
+        parse_octal(input).unwrap();
+    }
+
+    #[rstest]
+    #[case(b"")]
+    #[case(b"g")]
+    #[should_panic]
+    fn hex_failure(#[case] input: &[u8]) {
+        parse_hexadecimal_bigram(input).unwrap();
+    }
 }
