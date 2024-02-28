e76103bf262b17697fa96a07ca715bfa1a2f8380
Basile Dura <basile@bdura.me>
2024-02-28 10:21:35 +0100

    feat: regroup numbers, use new utilities

--- src/extraction/numbers.rs
+++ src/extraction/numbers.rs
@@ -1,44 +1,46 @@
-use nom::{
-    character::complete::digit1,
-    combinator::{opt, recognize},
-    sequence::tuple,
-    IResult,
-};
+use nom::IResult;
 
-use crate::utilities::parse_sign;
+use crate::utilities::{parse_integer, parse_real};
 
 use super::Extract;
 
 // TODO: handle parsing error.
-macro_rules! extract {
+macro_rules! extract_integer {
     ($type:ty) => {
         impl Extract for $type {
             fn extract(input: &[u8]) -> IResult<&[u8], Self> {
-                let (input, num) = recognize(tuple((opt(parse_sign), digit1)))(input)?;
-
-                // SAFETY: num is an optional sign, followed by digits.
-                let num = unsafe { std::str::from_utf8_unchecked(num) };
-
-                let n = num.parse().unwrap();
-
-                Ok((input, n))
+                parse_integer(input)
             }
         }
     };
 }
 
-extract!(u8);
-extract!(i8);
-extract!(u16);
-extract!(i16);
-extract!(u32);
-extract!(i32);
-extract!(u64);
-extract!(i64);
-extract!(usize);
-extract!(isize);
-extract!(u128);
-extract!(i128);
+// TODO: handle parsing error.
+macro_rules! extract_real {
+    ($type:ty) => {
+        impl Extract for $type {
+            fn extract(input: &[u8]) -> IResult<&[u8], Self> {
+                parse_real(input)
+            }
+        }
+    };
+}
+
+extract_integer!(u8);
+extract_integer!(i8);
+extract_integer!(u16);
+extract_integer!(i16);
+extract_integer!(u32);
+extract_integer!(i32);
+extract_integer!(u64);
+extract_integer!(i64);
+extract_integer!(usize);
+extract_integer!(isize);
+extract_integer!(u128);
+extract_integer!(i128);
+
+extract_real!(f32);
+extract_real!(f64);
 
 #[cfg(test)]
 mod tests {
@@ -50,7 +52,7 @@ mod tests {
     #[case(b"+1", 1)]
     #[case(b"0", 0)]
     #[case(b"+0", 0)]
-    fn parse_unsigned(#[case] input: &[u8], #[case] result: u128) {
+    fn unsigned_int(#[case] input: &[u8], #[case] result: u128) {
         assert_eq!(result, input.parse().unwrap());
 
         let r = result as u64;
@@ -76,7 +78,7 @@ mod tests {
     #[case(b"-0", 0)]
     #[case(b"-10", -10)]
     #[case(b"-100", -100)]
-    fn parse_signed(#[case] input: &[u8], #[case] result: i128) {
+    fn signed_int(#[case] input: &[u8], #[case] result: i128) {
         assert_eq!(result, input.parse().unwrap());
 
         let r = result as i64;
@@ -122,5 +124,21 @@ mod tests {
     #[case(b"-2147483648", i32::MIN)]
     fn parse_i32(#[case] input: &[u8], #[case] result: i32) {
         assert_eq!(result, input.parse().unwrap())
+    }
+
+    #[rstest]
+    #[case(b"1.", 1.0)]
+    #[case(b"+1.", 1.0)]
+    #[case(b"-1.", -1.0)]
+    #[case(b"-.1", -0.1)]
+    #[case(b".0", 0.0)]
+    #[case(b"+0.", 0.0)]
+    #[case(b"12345.6789", 12345.6789)]
+    #[case(b"-12345.6789", -12345.6789)]
+    fn reals(#[case] input: &[u8], #[case] result: f64) {
+        assert_eq!(result, input.parse().unwrap());
+
+        let r = result as f32;
+        assert_eq!(r, input.parse().unwrap());
     }
 }
