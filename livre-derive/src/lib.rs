bfff09f57f16c841c339c8f9e20dc8a607b588b8
Basile Dura <basile@bdura.me>
2024-03-01 11:53:02 +0100

    feat(derive): add FromDict derive. fix RawDict path

--- livre-derive/src/lib.rs
+++ livre-derive/src/lib.rs
@@ -8,7 +8,7 @@ mod attr;
 mod option;
 
 #[proc_macro_derive(Extract, attributes(livre))]
-pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn derive_extract(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // Parse the input tokens into a syntax tree.
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -27,6 +27,37 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         // The generated impl.
         impl<'input> ::livre_extraction::Extract<'input> for #name #ty_generics #where_clause {
             fn extract(input: &'input [u8]) -> ::nom::IResult<&'input [u8], Self> {
+                let (new_input, mut dict) = ::livre_extraction::RawDict::extract(input)?;
+
+                #extraction
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compiler.
+    proc_macro::TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(FromDict, attributes(livre))]
+pub fn derive_from_dict(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    // Parse the input tokens into a syntax tree.
+    let input = parse_macro_input!(input as DeriveInput);
+
+    // Used in the quasi-quotation below as `#name`.
+    let name = input.ident;
+
+    // Add a bound `T: Extract` to every type parameter T.
+    let generics = add_trait_bounds(input.generics);
+    let (_impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    // Generate an expression to sum up the heap size of each field.
+    let extraction = generate_extraction(&input.data);
+
+    let expanded = quote! {
+        use ::nom::error::ParseError;
+        // The generated impl.
+        impl<'input> ::livre_extraction::FromDict<'input> for #name #ty_generics #where_clause {
+            fn from_dict(dict: &mut ::livre_extraction::RawDict<'input>) -> ::livre_extraction::error::Result<Self> {
                 #extraction
             }
         }
@@ -72,8 +103,6 @@ fn generate_extraction(data: &Data) -> TokenStream {
                 .unwrap();
 
             quote! {
-                let (new_input, mut dict) = ::livre_extraction::dictionary::RawDict::extract(input)?;
-
                 #(
                     let #fieldname: #fieldty = dict
                         .#dictfunc(#fieldstr)
