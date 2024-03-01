e5e241dce7de7c8fb282bc22cd025cf32ea43df4
Basile Dura <basile@bdura.me>
2024-03-01 19:23:25 +0100

    fix: error with hash_map values
    added resolved tests

--- livre-extraction/src/utilities/raw_dict_value.rs
+++ livre-extraction/src/utilities/raw_dict_value.rs
@@ -1,4 +1,4 @@
-use livre_utilities::parse_dict_body;
+use livre_utilities::take_within_balanced;
 use nom::{
     branch::alt,
     bytes::complete::take_till,
@@ -14,7 +14,9 @@ pub struct RawValue<'input>(pub &'input [u8]);
 impl<'input> Extract<'input> for RawValue<'input> {
     fn extract(input: &'input [u8]) -> IResult<&'input [u8], Self> {
         let (input, value) = alt((
-            recognize(parse_dict_body),
+            recognize(take_within_balanced(b'[', b']')),
+            recognize(take_within_balanced(b'<', b'>')),
+            recognize(take_within_balanced(b'(', b')')),
             verify(take_till(|b| b == b'/'), |v: &[u8]| !v.is_empty()),
             recognize(Name::extract),
         ))(input)?;
@@ -35,6 +37,10 @@ mod tests {
     #[case(b"true ", b"true ")]
     #[case(b"true/", b"true")]
     #[case(b"/Value", b"/Value")]
+    #[case(
+        b"[<81b14aafa313db63dbd6f981e49f94f4>] ",
+        b"[<81b14aafa313db63dbd6f981e49f94f4>]"
+    )]
     #[case(b"<</Test true>>", b"<</Test true>>")]
     fn raw_value(#[case] input: &[u8], #[case] expected: &[u8]) {
         let (_, RawValue(v)) = RawValue::extract(input).unwrap();
