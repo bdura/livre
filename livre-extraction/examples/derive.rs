54e410cc3d2fe896448c21dd3d399e315235420a
Basile Dura <basile@bdura.me>
2024-03-03 14:01:48 +0100

    feat: reorganize FromDict trait
    treat HashMap as a whole type. like structs, it can implement FromDict

--- livre-extraction/examples/derive.rs
+++ livre-extraction/examples/derive.rs
@@ -1,6 +1,6 @@
-use livre_extraction::{extraction::Parse, Extract};
+use livre_extraction::{extraction::Parse, Extract, FromDict, RawDict};
 
-#[derive(Debug, Extract)]
+#[derive(Debug, Extract, FromDict)]
 struct Test {
     n: usize,
     prev: Option<String>,
@@ -10,6 +10,13 @@ fn main() {
     let input = b"<</N 12/Prev (test)>>";
 
     let test: Test = input.parse().unwrap();
+
+    println!("{:?}", test);
+    println!("{:?}", test.n);
+    println!("{:?}", test.prev);
+
+    let raw_dict: RawDict<'_> = input.parse().unwrap();
+    let test = Test::from_dict(raw_dict).unwrap();
 
     println!("{:?}", test);
     println!("{:?}", test.n);
