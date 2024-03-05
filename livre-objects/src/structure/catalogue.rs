4d0c011df3184967f55ec6d79bc3aa5ad3024e91
Basile Dura <basile@bdura.me>
2024-03-05 09:06:18 +0100

    test: catalogue

--- livre-objects/src/structure/catalogue.rs
+++ livre-objects/src/structure/catalogue.rs
@@ -1,8 +1,30 @@
-use livre_extraction::{FromDictRef, Reference};
+use livre_extraction::{Extract, FromDictRef, Reference};
 
-#[derive(Debug, PartialEq, Eq, FromDictRef)]
+#[derive(Debug, PartialEq, Eq, FromDictRef, Extract)]
 pub struct Catalogue {
     // pub version: Option<Name>,
     // pub extensions
     pub pages: Reference,
+}
+
+#[cfg(test)]
+mod tests {
+
+    use indoc::indoc;
+
+    use super::*;
+
+    #[test]
+    fn catalogue() {
+        let input = indoc! {b"
+            <</Type /Catalog
+                /Pages 2 0 R
+                /PageMode /UseOutlines
+                /Outlines 3 0 R
+            >>
+        "};
+
+        let (_, catalogue) = Catalogue::extract(input).unwrap();
+        assert_eq!(catalogue.pages, Reference::new(2, 0));
+    }
 }
