726e198674bee251b29289f547d233bff79ec9e1
Basile Dura <basile@bdura.me>
2024-03-12 22:51:37 +0100

    feat: use TypedReference if OptRef

--- livre-extraction/src/utilities/optional_reference.rs
+++ livre-extraction/src/utilities/optional_reference.rs
@@ -1,11 +1,11 @@
 use nom::{branch::alt, combinator::map, IResult};
 
-use crate::{Extract, Reference};
+use crate::{Extract, TypedReference};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum OptRef<T> {
     Val(T),
-    Ref(Reference),
+    Ref(TypedReference<T>),
 }
 
 impl<'input, T> Extract<'input> for OptRef<T>
@@ -14,7 +14,7 @@ where
 {
     fn extract(input: &'input [u8]) -> IResult<&'input [u8], Self> {
         alt((
-            map(Reference::extract, Self::Ref),
+            map(TypedReference::extract, Self::Ref),
             map(T::extract, Self::Val),
         ))(input)
     }
@@ -36,9 +36,9 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(b"0 1 R", OptRef::Ref(Reference::new(0, 1)))]
+    #[case(b"0 1 R", OptRef::Ref(TypedReference::new(0, 1)))]
     #[case(b"-12", OptRef::Val(-12))]
-    #[case(b"10 1 R", OptRef::Ref(Reference::new(10, 1)))]
+    #[case(b"10 1 R", OptRef::Ref(TypedReference::new(10, 1)))]
     fn maybe_array_i32(#[case] input: &[u8], #[case] expected: OptRef<i32>) {
         let (_, opt_ref) = OptRef::<i32>::extract(input).unwrap();
         assert_eq!(opt_ref, expected);
