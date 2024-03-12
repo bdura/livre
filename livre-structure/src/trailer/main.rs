fbc8b9109cbef7888e6ad1fc4b6cd7f1133bc1fe
Basile Dura <basile@bdura.me>
2024-03-12 18:13:44 +0100

    test: Trailer

--- livre-structure/src/trailer/main.rs
+++ livre-structure/src/trailer/main.rs
@@ -55,7 +55,10 @@ mod tests {
             <</Size 194/Root 1 0 R/Info 36 0 R/ID[<3D84D5219073404D8FCDF268E1ED1BF4><3D84D5219073404D8FCDF268E1ED1BF4>] /Prev 160714/XRefStm 160062>>
         "};
 
-        let (_, trailer) = parse_plain(input).unwrap();
+        let (_, direct) = parse_plain(input).unwrap();
+        let (_, trailer) = Trailer::extract(input).unwrap();
+
+        assert_eq!(direct, trailer);
 
         let Trailer { dict, refs } = trailer;
 
@@ -65,5 +68,24 @@ mod tests {
         assert_eq!(dict.root, Reference::new(1, 0));
         assert_eq!(dict.info, Reference::new(36, 0));
         assert_eq!(dict.prev, Some(160714));
+    }
+
+    #[test]
+    fn xref_stream_trailer() {
+        let input = include_bytes!("../../../tests/objects/trailer_xref_stream.bin");
+
+        let (_, direct) = parse_stream(input).unwrap();
+        let (_, trailer) = Trailer::extract(input).unwrap();
+
+        assert_eq!(direct, trailer);
+
+        let Trailer { dict, refs } = trailer;
+
+        assert_eq!(refs.len(), 2010);
+
+        assert_eq!(dict.size, 92813);
+        assert_eq!(dict.root, Reference::new(90794, 0));
+        assert_eq!(dict.info, Reference::new(90792, 0));
+        assert_eq!(dict.prev, Some(116));
     }
 }
