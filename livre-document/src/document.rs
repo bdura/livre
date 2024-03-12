64ec8119edcd27e11b0d094a5941bfb317909b24
Basile Dura <basile@bdura.me>
2024-03-12 22:52:04 +0100

    feat: adapt document and example

--- livre-document/src/document.rs
+++ livre-document/src/document.rs
@@ -1,13 +1,13 @@
 use std::collections::HashMap;
 
-use livre_extraction::Extract;
+use livre_extraction::{Extract, TypedReference};
 use nom::{multi::many1, IResult};
 
 use livre_objects::Reference;
 
 use livre_utilities::take_whitespace;
 
-use livre_structure::{RefLocation, StartXRef, Trailer};
+use livre_structure::{Catalogue, RefLocation, StartXRef, Trailer};
 
 use crate::{Header, Update};
 
@@ -16,8 +16,19 @@ pub struct Document<'input> {
     pub header: Header,
     pub body: HashMap<Reference, &'input [u8]>,
     pub crossrefs: HashMap<Reference, RefLocation>,
-    pub root: Reference,
+    pub root: TypedReference<Catalogue>,
     pub startxref: StartXRef,
+}
+
+impl Document<'_> {
+    pub fn get_location(&self, reference: impl Into<Reference>) -> Option<RefLocation> {
+        let reference = reference.into();
+        self.crossrefs.get(&reference).copied()
+    }
+    pub fn get_referenced_bytes(&self, reference: impl Into<Reference>) -> Option<&[u8]> {
+        let reference = reference.into();
+        self.body.get(&reference).copied()
+    }
 }
 
 impl<'input> Extract<'input> for Document<'input> {
