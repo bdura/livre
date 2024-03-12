520d68922388b02d3586d3180bc46a647bed0609
Basile Dura <basile@bdura.me>
2024-03-12 11:42:11 +0100

    docs: doc example

--- livre-document/examples/doc.rs
+++ livre-document/examples/doc.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use livre_document::Document;
-use livre_extraction::{extract, Extract, IntoString, NoOp};
+use livre_extraction::{extract, Extract, NoOp};
 use livre_objects::Stream;
 use livre_structure::{Catalogue, Node, Page};
 
@@ -25,18 +25,28 @@ fn main() {
     let (_, root) = extract::<Catalogue>(root_raw).unwrap();
 
     let &pages_raw = doc.body.get(&root.pages).unwrap();
-    let (_, pages) = extract::<Node>(pages_raw).unwrap();
     // let (_, IntoString(pages)) = extract(pages_raw).unwrap();
+    // println!("{pages}");
+    let (_, pages) = extract::<Node>(pages_raw).unwrap();
+    // println!("{pages:?}");
 
     let &page_raw = doc.body.get(&pages.kids[0]).unwrap();
-    let (_, page) = extract::<Page>(page_raw).unwrap();
     // let (_, IntoString(page)) = extract(page_raw).unwrap();
+    let (_, page) = extract::<Page>(page_raw).unwrap();
 
     let &content_raw = doc.body.get(&page.contents.unwrap().0[0]).unwrap();
     // let (_, IntoString(content)) = extract(content_raw).unwrap();
     let (_, content) = extract::<Stream<'_, NoOp>>(content_raw).unwrap();
     let decoded = content.decode().unwrap();
     let decoded = String::from_utf8_lossy(&decoded);
+    // println!("{decoded}");
 
-    println!("{decoded}");
+    for line in decoded
+        .split('\n')
+        .filter(|t| t.to_lowercase().contains("tj"))
+    {
+        println!("{line:?}");
+    }
+
+    // println!("{decoded}");
 }
