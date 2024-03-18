601a8dacbd3d77387616e22a35d9c7f95e5a8393
Basile Dura <basile@bdura.me>
2024-03-18 17:26:39 +0100

    fix: remove unused error types

--- livre-extraction/src/error.rs
+++ livre-extraction/src/error.rs
@@ -6,15 +6,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ExtractionError {
-    #[error("Unexpected type: expected {expected}, got {got}")]
-    UnexpectedType {
-        expected: &'static str,
-        got: &'static str,
-    },
     #[error("Key {0} not found")]
     KeyNotFound(String),
-    #[error("Filter decode error")]
-    FilterDecode(#[from] std::io::Error),
     #[error("unknown error.")]
     Unknown,
 }
