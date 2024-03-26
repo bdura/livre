ade513db6fbc69268ed0fe3d4c148541aba6919e
Basile Dura <basile@bdura.me>
2024-03-26 11:42:05 +0100

    feat: use Cow<'_, str> as middleware for String

--- livre-extraction/src/primitives/string.rs
+++ livre-extraction/src/primitives/string.rs
@@ -1,10 +1,23 @@
+use std::borrow::Cow;
+
 use crate::{encoding::decode_str, extract, extraction::Extract, LitBytes};
+
+impl<'input> Extract<'input> for Cow<'input, str> {
+    fn extract(input: &'input [u8]) -> nom::IResult<&'input [u8], Self> {
+        let (input, LitBytes(bytes)) = extract(input)?;
+
+        let res = match bytes {
+            Cow::Borrowed(input) => decode_str(input),
+            Cow::Owned(input) => Cow::Owned(decode_str(&input).to_string()),
+        };
+        Ok((input, res))
+    }
+}
 
 impl Extract<'_> for String {
     fn extract(input: &[u8]) -> nom::IResult<&[u8], Self> {
-        let (input, LitBytes(bytes)) = extract(input)?;
-        let res = decode_str(&bytes);
-        Ok((input, res.to_string()))
+        let (input, cow) = Cow::<'_, str>::extract(input)?;
+        Ok((input, cow.to_string()))
     }
 }
 
@@ -14,7 +27,6 @@ mod tests {
     use rstest::rstest;
 
     use super::*;
-    use crate::parse;
 
     #[rstest]
     #[case(b"(abcd)", "abcd")]
@@ -24,9 +36,8 @@ mod tests {
     #[case(b"(\\0533)", "+3")]
     #[case(b"(te\\\\st)", "te\\st")]
     #[case(b"(te\\\nst)", "test")]
-    fn string(#[case] input: &[u8], #[case] expected: &str) {
+    fn pdf_encoding(#[case] input: &[u8], #[case] expected: &str) {
         let (_, parsed) = String::extract(input).unwrap();
         assert_eq!(parsed, expected);
-        assert_eq!(parse::<String>(input).unwrap(), expected);
     }
 }
