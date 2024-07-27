0f5e05f8d94f26de25a448ff4df2f755b9d7bb99
Basile Dura <basile@bdura.me>
2024-07-27 18:28:11 +0200

    feat: use livre-serde for filters

--- livre-filters/src/lib.rs
+++ livre-filters/src/lib.rs
@@ -1,48 +1,69 @@
 //! Filters for PDF stream objects.
 
+use std::borrow::Cow;
+
 use enum_dispatch::enum_dispatch;
 
 mod error;
 pub use error::Result;
 
 mod filters;
-pub use filters::{DCTDecode, FlateDecode};
-use livre_extraction::{Extract, IResult, Name};
+use filters::{DCTDecode, FlateDecode};
+use serde::Deserialize;
 
 #[enum_dispatch]
 pub trait Filtering {
     fn decode(&self, bytes: &[u8]) -> Result<Vec<u8>>;
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Deserialize)]
+enum FilterName {
+    FlateDecode,
+    DCTDecode,
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[serde(from = "FilterName")]
 #[enum_dispatch(Filtering)]
 pub enum Filter {
     FlateDecode(FlateDecode),
     DCTDecode(DCTDecode),
 }
 
-impl Filter {
-    pub fn from_name(name: &str) -> Self {
-        match name {
-            "ASCIIHexDecode" => todo!(),
-            "ASCII85Decode" => todo!(),
-            "LZWDecode" => todo!(),
-            "FlateDecode" => FlateDecode.into(),
-            "RunLengthDecode" => todo!(),
-            "CCITTFaxDecode" => todo!(),
-            "JBIG2Decode" => todo!(),
-            "DCTDecode" => DCTDecode.into(),
-            "JPXDecode" => todo!(),
-            "Crypt" => todo!(),
-            _ => unreachable!("Unknown filter."),
+impl<T: Filtering> Filtering for Vec<T> {
+    fn decode(&self, bytes: &[u8]) -> Result<Vec<u8>> {
+        let mut cow = Cow::Borrowed(bytes);
+        for decoder in self {
+            let result = decoder.decode(cow.as_ref())?;
+            cow = Cow::Owned(result);
+        }
+        Ok(cow.into_owned())
+    }
+}
+
+impl From<FilterName> for Filter {
+    fn from(value: FilterName) -> Self {
+        match value {
+            FilterName::FlateDecode => Self::FlateDecode(FlateDecode),
+            FilterName::DCTDecode => Self::DCTDecode(DCTDecode),
         }
     }
 }
 
-impl<'input> Extract<'input> for Filter {
-    fn extract(input: &'input [u8]) -> IResult<&'input [u8], Self> {
-        let (input, Name(name)) = Name::extract(input)?;
-        let filter = Filter::from_name(&name);
-        Ok((input, filter))
+#[cfg(test)]
+mod tests {
+    use livre_serde::from_bytes;
+
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(b"/FlateDecode", FlateDecode.into())]
+    #[case(b"  /FlateDecode", FlateDecode.into())]
+    #[case(b"/DCTDecode", DCTDecode.into())]
+    #[should_panic]
+    #[case(b"/NonExistent", DCTDecode.into())]
+    fn filter_deserialize(#[case] input: &[u8], #[case] expected: Filter) {
+        assert_eq!(expected, from_bytes(input).unwrap());
     }
 }
