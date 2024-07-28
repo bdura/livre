900b75c963a18c72e419d87dc131d5ad323349d7
Basile Dura <basile@bdura.me>
2024-07-29 01:13:22 +0200

    feat: clean fonts

--- livre-fonts/src/lib.rs
+++ livre-fonts/src/lib.rs
@@ -3,8 +3,11 @@ use enum_dispatch::enum_dispatch;
 mod descriptors;
 mod type1;
 
+pub use type1::Type1FontDict;
+
 pub use descriptors::{FontDescriptor, FontFlags};
 use livre_data::Rectangle;
+use serde::Deserialize;
 
 #[enum_dispatch]
 pub trait FontBehavior {
@@ -12,9 +15,12 @@ pub trait FontBehavior {
     fn convert(&self, string_input: &[u8]) -> (String, Rectangle);
 }
 
+#[derive(Debug, PartialEq, Deserialize)]
+#[serde(tag = "Subtype")]
 pub enum Font {
     // Type0,
-    Type1,
+    Type1(Type1FontDict),
+    MMType1(Type1FontDict),
     // Type3,
     // TrueType,
     // CIDFont,
