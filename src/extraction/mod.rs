12d41264ebaf6e5714d1fa545fd9a3c147ef7bbc
Basile Dura <basile@bdura.me>
2024-08-27 17:41:43 +0200

    docs: add light documentation for extraction of primitive types

--- src/extraction/mod.rs
+++ src/extraction/mod.rs
@@ -1,14 +1,27 @@
-use winnow::{BStr, PResult};
+//! Livre organises the entire parsing logic around the [`Extract`] trait,
+//! which defines a way for the type to extract itself from a stream of bytes,
+//! consuming the input.
+
+use winnow::{BStr, PResult, Parser};
 
 mod primitives;
 mod utilities;
 
 /// The [`Extract`] trait marks a type as extractable from a stream of bytes,
-/// without any context.
+/// without any context. Not object safe.
 ///
 /// TODO: add a `Build` trait that can follow references as they arise
 pub trait Extract<'de>: Sized {
     fn extract(input: &mut &'de BStr) -> PResult<Self>;
+
+    /// Consume the input, without trying to parse. Especially useful for
+    /// struct/map parsing, since we just need to extract the *bytes* associated
+    /// with the type.
+    ///
+    /// Some types (if not all) may benefit from using a dedicated logic.
+    fn recognize(input: &mut &'de BStr) -> PResult<&'de [u8]> {
+        Self::extract.take().parse_next(input)
+    }
 }
 
 /// Direct extraction. Most of the time the type can be inferred from
--- src/extraction/primitives/mod.rs
+++ src/extraction/primitives/mod.rs
@@ -1,3 +1,8 @@
+//! Implementation of the [`Extract`] trait for primitive types: `bool`, (un-)signed integers,
+//! floating point numbers, unit type, vectors, arrays, optionals & tuple.
+//!
+//! Note that we do not implement the trait for `String`, because of the way PDFs represent text.
+
 use super::Extract;
 
 mod array;
