fbbcbb3bcc0e2d54a9fd14c0ed1243970e88a691
Basile Dura <basile@bdura.me>
2025-01-12 14:11:32 +0100

    feat: follow object stream linked list

--- src/structure/stream.rs
+++ src/structure/stream.rs
@@ -30,15 +30,21 @@ use crate::{
 /// that is, through a separate `ObjectStream` that extends the one that contains it.
 ///
 /// For now, we are making the assumption that a reference stream will always point to the correct
-/// `ObjectStream`.
+/// `ObjectStream`, and do not instantiate the full object stream. However, in case this assumption
+/// is wrong, we keep the PDF reference around to instantiate it if need be. Note that this option
+/// is only available to owned types (see [`build_owned_object`](Self::build_owned_object)) for
+/// more detail.
 pub struct ObjectStream {
     /// Mapping from ReferenceId to byte offset within the content
     map: HashMap<ReferenceId, usize>,
     /// Content decoded from the stream, stripped of the header
     content: Vec<u8>,
+    /// Optional reference to a previous ObjectStream.
+    extends: Option<Reference<ObjectStream>>,
 }
 
 impl ObjectStream {
+    /// Get the byte stream associated with a reference.
     fn get_data(&self, reference: &ReferenceId) -> Option<&BStr> {
         self.map
             .get(reference)
@@ -47,6 +53,7 @@ impl ObjectStream {
 }
 
 impl<'de> ObjectStream {
+    /// Build an object contained within the `ObjectStream`. Returns an error if the key is absent.
     pub fn build_object<B, T>(&'de self, reference: &ReferenceId, builder: &B) -> PResult<T>
     where
         T: Build<'de>,
@@ -60,6 +67,27 @@ impl<'de> ObjectStream {
     }
 }
 
+impl ObjectStream {
+    /// Owned types can be built following the linked list of `ObjectStream` if need be.
+    ///
+    /// This is only available to owned types because while following the linked list, we
+    /// instantiate transient objects that cannot be referenced into.
+    pub fn build_owned_object<B, T>(&self, reference: &ReferenceId, builder: &B) -> PResult<T>
+    where
+        T: for<'de> Build<'de>,
+        B: for<'de> Builder<'de>,
+    {
+        if let Some(mut input) = self.get_data(reference) {
+            builder.as_parser().parse_next(&mut input)
+        } else if let Some(extends) = self.extends {
+            let stream = builder.build_reference(extends)?;
+            stream.build_object(reference, builder)
+        } else {
+            Err(ErrMode::Backtrack(ContextError::new()))
+        }
+    }
+}
+
 impl<'de> Build<'de> for ObjectStream {
     fn build<B>(input: &mut &'de BStr, builder: &B) -> PResult<Self>
     where
@@ -67,14 +95,7 @@ impl<'de> Build<'de> for ObjectStream {
     {
         trace("livre-object-stream", move |i: &mut &'de BStr| {
             let Stream {
-                structured:
-                    ObjectStreamDict {
-                        n,
-                        first,
-                        // NOTE: for now we make the strong assumption that stream extension is in fact
-                        // unnecessary. See notes section in `ObjectStream` docs.
-                        extends: _,
-                    },
+                structured: ObjectStreamDict { n, first, extends },
                 content,
             } = builder.as_parser().parse_next(i)?;
 
@@ -89,7 +110,11 @@ impl<'de> Build<'de> for ObjectStream {
                 .collect();
             it.finish()?;
 
-            Ok(Self { map, content })
+            Ok(Self {
+                map,
+                content,
+                extends,
+            })
         })
         .parse_next(input)
     }
@@ -103,11 +128,7 @@ struct ObjectStreamDict {
     /// Byte offset of the **first** serialised element in the object stream decoded content.
     pub first: usize,
     /// An optional reference to another object stream.
-    #[allow(
-        dead_code,
-        reason = "It looks like the extension is not actually needed, but we would still like to use it."
-    )]
-    pub extends: Option<Reference<()>>,
+    pub extends: Option<Reference<ObjectStream>>,
 }
 
 impl<'de> BuildFromRawDict<'de> for ObjectStreamDict {
