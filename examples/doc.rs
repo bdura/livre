ef4778a8c49df0982b870d1c698e3dff07d54ab2
Basile Dura <basile@bdura.me>
2025-02-10 01:10:27 +0100

    docs: update document example

--- examples/doc.rs
+++ examples/doc.rs
@@ -1,28 +1,17 @@
-use livre::{
-    extraction::{Extract, Object},
-    follow_refs::Builder,
-    InMemoryDocument,
-};
+use livre::{extraction::Extract, InMemoryDocument};
 use winnow::BStr;
 
 fn main() {
-    let mut letter: &BStr = include_bytes!("../tests/letter.pdf").as_slice().as_ref();
+    //let mut letter: &BStr = include_bytes!("../tests/letter.pdf").as_slice().as_ref();
+    let mut letter: &BStr = include_bytes!("../tests/text.pdf").as_slice().as_ref();
     //let mut letter: &BStr = include_bytes!("../resource/ISO_32000-2-2020_sponsored.pdf").as_slice().as_ref();
-    //let mut letter: &BStr = include_bytes!("../tests/text.pdf").as_slice().as_ref();
 
     let doc = InMemoryDocument::extract(&mut letter).unwrap();
 
-    for &ref_id in doc.xrefs.keys() {
+    for page in doc.catalog.pages.iter() {
         println!(
-            "{:?}\n{:#?}\n\n",
-            ref_id,
-            &doc.build_reference::<Object>(ref_id.into())
+            "{}",
+            String::from_utf8_lossy(&page.build_content(&doc).unwrap())
         )
     }
-
-    //let catalog = doc.build_reference(doc.catalog).unwrap();
-    //
-    //for page in &catalog.pages.pages {
-    //    println!("{page:?}");
-    //}
 }
