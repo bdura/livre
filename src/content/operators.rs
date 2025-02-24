8ba040ea4e9095c3fe5d19fea5fe8e69e423ae9f
Basile Dura <basile@bdura.me>
2025-02-25 00:06:30 +0100

    feat: update operators module

--- src/content/operators.rs
+++ src/content/operators.rs
@@ -1,4 +1,4 @@
-use crate::extraction::Name;
+use crate::extraction::{LiteralString, Name};
 
 pub enum RenderingMode {
     /// Fill text.
@@ -19,22 +19,23 @@ pub enum RenderingMode {
     AddTextAndClip,
 }
 
+// TODO: create dedicated types for each operator, for better behaviour locality.
 pub enum Operator {
-    /// Operator `Tc`.
+    /// `Tc` operator.
     /// Set the caracter spacing, $T_c$, to a number expressed in unscaled text space units.
     SetCharacterSpacing(f32),
-    /// Operator `Tw`
+    /// `Tw` operator.
     /// Unscaled text space units.
     SetWordSpacing(f32),
-    /// Operator `Tz`
+    /// `Tz` operator.
     SetHorizontalScaling(f32),
-    /// Operator `TL`.
+    /// `TL` operator.
     SetTextLeading(f32),
-    /// Operator `Tf`
+    /// `Tf` operator.
     SetFontAndFontSize(Name, f32),
-    /// Operator `Tr`
+    /// `Tr` operator.
     SetTextRenderingMode(RenderingMode),
-    /// Operator `Ts`.
+    /// `Ts` operator.
     SetTextRise(f32),
     /// `BT` operator.
     ///
@@ -47,4 +48,62 @@ pub enum Operator {
     ///
     /// End a text object, discarding the text matrix.
     EndText,
+    /// `Td` operator.
+    ///
+    /// Move to the start of the next line, offset from the start of the current line
+    /// by `(tx, ty)`. `tx` and `ty` shall denote numbers expressed in unscaled
+    /// text space units
+    MoveByOffset(f32, f32),
+    /// `TD` operator.
+    ///
+    /// Move to the start of the next line, offset from the start of the current line
+    /// by `(tx, ty)`. As a side effect, this operator shall set the leading parameter
+    /// in the text state. Equivalent to:
+    ///
+    /// ```raw
+    /// -ty TL
+    /// tx ty Td
+    /// ```
+    MoveByOffsetAndSetLeading(f32, f32),
+    /// `Tm` operator.
+    SetTextMatrix(f32, f32, f32, f32, f32, f32),
+    /// `T*` operator
+    ///
+    /// Move to the start of the next line. Equivalent to:
+    ///
+    /// ```raw
+    /// 0 -T_l Td
+    /// ```
+    ///
+    /// where `T_l` denotes the current leading parameter in the text state.
+    /// The negative of T l is used here because T l is the text leading expressed
+    /// as a positive number. Going to the next line entails decreasing the y coordinate.
+    MoveToNextLine,
+    /// `Tj` operator. Show a text string.
+    ShowText(LiteralString),
+    /// `'` operator. Equivalent to:
+    ///
+    /// ```raw
+    /// T*
+    /// string Tj
+    /// ```
+    MoveToNextLineAndShowText(LiteralString),
+    /// `"` operator.
+    ///
+    /// Move to the next line and show a text string, using `aw` as the word spacing and
+    /// `ac` as the character spacing (setting the corresponding parameters in the text state).
+    /// `aw` and `ac` shall be numbers expressed in unscaled text space units.
+    MoveToNextLineAndShowTextWithSpacing(f32, f32, LiteralString),
+    /// `TJ` operator.
+    ///
+    /// Show zero or more text strings, allowing individual glyph positioning.
+    /// Each element of the array is either a string or a number:
+    ///
+    /// - in the case of a string, the operator shows the text;
+    /// - in the case of a number, the operator adjust the text position by that
+    ///   amount (i.e. translate the text matrix). Expressed in **thousandths of a unit of
+    ///   text space.
+    ///   That amount is substracted from the current "selected coordinate",
+    ///   depending on the writing mode.
+    ShowTextArray, // TODO: add argument.
 }
