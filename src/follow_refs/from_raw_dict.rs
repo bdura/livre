9dabe0743f376fe17094e381ce3e0ad436a609d9
Basile Dura <basile@bdura.me>
2025-03-14 00:45:30 +0100

    docs: light documentation change

--- src/follow_refs/from_raw_dict.rs
+++ src/follow_refs/from_raw_dict.rs
@@ -11,7 +11,7 @@ use super::{Build, Builder};
 ///
 /// This indirection in a type's ability to [`Build`] itself lets us define more complex extraction
 /// strategies, in particular derivable ones. Since a `BuildFromRawDict` type merely pops relevant
-/// keys from a mutable reference to a [`RawDict`], we give more structure to otherwise flat
+/// keys from a mutable reference to a [`RawDict`], we can give more structure to otherwise flat
 /// dictionary structures.
 pub trait BuildFromRawDict: Sized {
     fn build_from_raw_dict<B>(dict: &mut RawDict<'_>, builder: &B) -> PResult<Self>
