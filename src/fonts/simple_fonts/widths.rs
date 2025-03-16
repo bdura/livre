5cefce6011f567cff896133dab6d2fde73863798
Basile Dura <basile@bdura.me>
2025-03-16 23:09:03 +0100

    feat: add Widths

---
+++ src/fonts/simple_fonts/widths.rs
@@ -0,0 +1,27 @@
+use livre_derive::BuildFromRawDict;
+
+#[derive(Debug, PartialEq, Clone, BuildFromRawDict)]
+pub struct Widths {
+    /// The first character code defined in the font’s `widths` array
+    pub first_char: usize,
+    /// The last character code defined in the font’s `widths` array
+    pub last_char: usize,
+    /// An array of `last_char - first_char + 1` numbers, each element being
+    /// the glyph width for the character code that equals FirstChar plus the array index.
+    /// For character codes outside the range `first_char` to `last_char`, the value of
+    /// `missing_width` from the FontDescriptor entry for this font shall be used. The
+    /// glyph widths shall be measured in units in which 1000 units correspond to 1 unit
+    /// in text space.
+    pub widths: Vec<u16>,
+}
+
+impl Widths {
+    /// Provides the width for a single glyph.
+    pub fn width(&self, cid: usize) -> Option<u16> {
+        if cid < self.first_char {
+            None
+        } else {
+            self.widths.get(cid - self.first_char).copied()
+        }
+    }
+}
