e69f3f73e11cd2ac7b77b8322938e3d2a5e799f6
Basile Dura <basile@bdura.me>
2025-03-30 04:32:40 +0200

    feat: add encodings
    use lopdf's approach

---
+++ src/fonts/encoding/mod.rs
@@ -0,0 +1,16 @@
+//! PDF encodings.
+
+use enum_dispatch::enum_dispatch;
+
+mod builtins;
+pub use builtins::{
+    BuiltInEncoding, ExpertEncoding, MacExpertEncoding, MacRomanEncoding, PdfDocEncoding,
+    StandardEncoding, WinAnsiEncoding,
+};
+
+/// A font's encoding is the association between character codes and glyph description.
+#[enum_dispatch]
+pub trait Encoding {
+    /// Convert a code into a character.
+    fn to_char(&self, code: u8) -> u16;
+}
