c404dcce747eee1239f207dafddd95e4f6522ed4
Basile Dura <basile@bdura.me>
2025-03-30 04:34:08 +0200

    docs: update documentation for fonts

---
+++ src/fonts/composite_fonts/mod.rs
@@ -0,0 +1,7 @@
+//! Composite fonts.
+//!
+//! > With a composite font (PDF 1.2), multiple-byte codes may be used
+//! > to select glyphs. In this instance, one or more consecutive bytes
+//! > of the string shall be treated as a single character code. The code
+//! > lengths and the mappings from codes to glyphs are defined in a data
+//! > structure called a CMap, described in 9.7, "Composite fonts".
